bef82807fb8dec3206b9f1c711911c029f81cb85
Suporte 04 <suporte04@atsditsolutions.com>
2025-03-20 20:03:54 -0300

    first commit

---
+++ src/main/java/org/projetoverdinho/projetoverdinhoapi/ProjetoVerdinhoApiApplication.java
@@ -0,0 +1,15 @@
+package org.projetoverdinho.projetoverdinhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVerdinhoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoVerdinhoApiApplication.class, args);
+
+        System.out.println("hello world");
+    }
+
+}
