532a6c7f8299bb443741fe2fe52198d12f529dce
bobonimo111 <carlosmaycon443@gmail.com>
2025-03-25 16:04:52 -0300

    criado interface de conecção com o banco de dados

--- src/main/java/org/projetoverdinho/projetoverdinhoapi/repository/UserRepository.java
+++ src/main/java/org/projetoverdinho/projetoverdinhoapi/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package org.projetoverdinho.projetoverdinhoapi.repository;
 
-public interface UserRepository {
+import org.projetoverdinho.projetoverdinhoapi.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
 }
