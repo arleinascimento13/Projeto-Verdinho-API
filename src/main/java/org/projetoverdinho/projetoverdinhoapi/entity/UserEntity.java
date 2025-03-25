e442df8cbc00f905bc1b7b85bbd99384df782e63
bobonimo111 <carlosmaycon443@gmail.com>
2025-03-25 14:53:11 -0300

    Criado o gerenciado de entidades no banco de dados

---
+++ src/main/java/org/projetoverdinho/projetoverdinhoapi/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package org.projetoverdinho.projetoverdinhoapi.entity;
+
+import jakarta.persistence.*;
+
+import org.projetoverdinho.projetoverdinhoapi.enumerator.Role;
+
+@Entity
+@Table(name = "tb_user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+    @Column(name = "role")
+    private Role role;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "sector")
+    private String sector;
+}
