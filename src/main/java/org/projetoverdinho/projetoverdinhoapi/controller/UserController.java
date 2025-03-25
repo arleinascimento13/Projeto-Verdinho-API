5fb36c752b1305d773869ac89ff02d9fe20fc0ab
bobonimo111 <carlosmaycon443@gmail.com>
2025-03-25 14:15:58 -0300

    Iniciado a crição de o controller para usuarios

--- src/main/java/org/projetoverdinho/projetoverdinhoapi/controller/UserController.java
+++ src/main/java/org/projetoverdinho/projetoverdinhoapi/controller/UserController.java
@@ -1,4 +1,34 @@
 package org.projetoverdinho.projetoverdinhoapi.controller;
 
+import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private UserController controller;
+
+
+
+    @PostMapping
+    public String create(){
+        return null;
+    }
+
+    @GetMapping
+    public String list(){
+        return null;
+    }
+
+    @PutMapping
+    public  String update(){
+        return null;
+    }
+
+    @DeleteMapping
+    public String delete(){
+        return null;
+    }
 }
