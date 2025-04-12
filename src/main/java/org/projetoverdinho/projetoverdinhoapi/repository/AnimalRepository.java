b6fca48fdb1256e7c62b38526d763bc09adac11f
bobonimo <carlosmaycon443@gmail.com>
2025-04-12 16:09:24 -0300

    adicionado repositorio para as classes

---
+++ src/main/java/org/projetoverdinho/projetoverdinhoapi/repository/AnimalRepository.java
@@ -0,0 +1,6 @@
+package org.projetoverdinho.projetoverdinhoapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimalRepository extends JpaRepository<AnimalRepository,Integer> {
+}
